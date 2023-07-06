619e640190067668746fc6b0d577d41bb18f4805
Razvan Moraru <redcom@gmail.com>
2023-07-06 20:44:08 +0200

    Update script to package and publish helm charts
    Signed-off-by: Razvan Moraru <redcom@gmail.com>

--- scripts/helm-package.sh
+++ scripts/helm-package.sh
@@ -14,11 +14,11 @@ charts_source_folder="./helm-charts"
 for i in $(ls $charts_source_folder)
 do
   helm lint ./helm-charts/$i
-  helm package ./helm-charts/$i -d ./build
+  helm package ./helm-charts/$i -d ./docs
 done
 
 # index the repo
-helm repo index --url https://$repo_owner.github.io/$repo_name/  ./build
+helm repo index --url https://$repo_owner.github.io/$pages_branch_name/  ./docs
 
 # check if index file was created
-[[ $(wc -l ./build/index.yaml | awk '{print $1}') -gt 3 ]] && echo "Index file is not empty" || echo "Index file is empty"
+[[ $(wc -l ./docs/index.yaml | awk '{print $1}') -gt 3 ]] && echo "Index file is not empty" || echo "Index file is empty"
--- scripts/publish-charts.sh
+++ scripts/publish-charts.sh
@@ -8,9 +8,27 @@ source ./scripts/.env
 pages_branch_name=$PAGES_BRANCH_NAME
 repo_owner=$REPO_OWNER
 repo_name=$REPO_NAME
-charts_source_folder="./helm-charts"
 
 # check if build folder is not empty
-[ "$(ls -A ./build)" ] && echo "Build folder is not empty" || echo "Build folder is empty"
+[ "$(ls -A ./docs/index.yaml)" ] && echo "Build folder is not empty" || echo "Build folder is empty"
+
+# checkout the heml release branch
+# Get the current branch name and save it in a variable
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# hide local changes in current branch
+git stash
+
+git checkout $pages_branch_name
+git pull origin $pages_branch_name
+git add ./docs
+git commit -m "Update Helm charts"
+git push origin $pages_branch_name
+
+# Switching back to the saved branch: $current_branch"
+git checkout "$current_branch"
+
+# unstash local changes
+git stash pop
 
 
